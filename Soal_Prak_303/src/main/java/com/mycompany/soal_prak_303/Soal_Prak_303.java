649390cf318c6c9a49e60bdd349da11ba5e6caaa
nandaoke007 <87602178+nandaoke007@users.noreply.github.com>
2023-06-24 23:57:32 +0700

    Add files via upload

---
+++ Soal_Prak_303/src/main/java/com/mycompany/soal_prak_303/Soal_Prak_303.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.soal_prak_303;
+import java.util.Scanner;
+
+public class Soal_Prak_303 {
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       int bil, awal, akhir;
+       
+       System.out.println("PROGRAM JAVA MENAMPILKAN DERET BILANGAN PRIMA");
+       System.out.print("Mulai dari : ");
+       awal=input.nextInt();
+       System.out.print("Sampai : ");
+       akhir=input.nextInt();
+       System.out.println("==============================================");
+       
+       for (int i=awal; i<=akhir; i++){
+            bil=0;
+            for (int j=1;j<=i;j++){
+                if (i%j==0){
+                    bil=bil+1;
+                }
+            }
+            if (bil==2){
+                System.out.print(i+" ");
+            }
+       }
+    }
+}
